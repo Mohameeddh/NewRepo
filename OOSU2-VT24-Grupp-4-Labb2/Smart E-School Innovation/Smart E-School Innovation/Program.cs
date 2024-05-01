230c153b3c1c63d665eb05ba23eacd0de0beccc7
Mohameeddh <s2106555@student.hb.se>
2024-05-01 14:44:49 +0200

    Add project files.

---
+++ OOSU2-VT24-Grupp-4-Labb2/Smart E-School Innovation/Smart E-School Innovation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Smart_E_School_Innovation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ OOSU2-VT24-Grupp-4-Labb2/WpfApp222222/WpfApp222222/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace WpfApp222222
+{
+    public partial class MainWindow : Window
+    {
+        bool running = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+            tbHello.Text = "Running";
+            running = true;
+        }
+    }
+}
