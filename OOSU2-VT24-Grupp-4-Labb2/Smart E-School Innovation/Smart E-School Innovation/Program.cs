e1b591219981b57a855d0cfdc949f8270073a335
Mohameeddh <s2106555@student.hb.se>
2024-05-05 18:07:21 +0200

    OpenAI

--- OOSU2-VT24-Grupp-4-Labb2/Smart E-School Innovation/Smart E-School Innovation/Program.cs
+++ OOSU2-VT24-Grupp-4-Labb2/Smart E-School Innovation/Smart E-School Innovation/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Smart_E_School_Innovation
+﻿using Microsoft.Extensions.Configuration;
+using OpenAI_API;
+
+var configuration = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+var key = configuration["OpenAI_Key"];
+
+if (string.IsNullOrEmpty(key))
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, wwww666!");
-        }
-    }
+    Console.WriteLine("OpenAI nyckel saknas");
+    return;
 }
+///HH
+
+var api = new OpenAIAPI(key);
+
+var result = await api.Chat.CreateChatCompletionAsync("Förklara vad en pitch är?");
+
+Console.WriteLine(result);
