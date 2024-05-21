2c620523f2453590c5c81bf2b8b7385e7a8e06ed
Mohameeddh <s2106555@student.hb.se>
2024-05-21 18:56:47 +0200

    546

---
+++ OOSU2-VT24-Grupp-4-Labb2/Smart E-School Innovation/PresentationsLager/Alternativen.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace PresentationsLager
+{
+    public partial class Alternativen : Window
+    {
+        public Alternativen()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenereraFragor_Click(object sender, RoutedEventArgs e)
+        {
+            FrågeGenerator frågeGenerator = new FrågeGenerator();
+            frågeGenerator.Show();
+            this.Close();
+        }
+
+        private void btnSammanfattaKapitel_Click(object sender, RoutedEventArgs e)
+        {
+            // Här kan du lägga till kod för att öppna fönstret eller funktionen för att sammanfatta kapitel
+            MessageBox.Show("Sammanfatta kapitel-funktionen är ännu inte implementerad.");
+            this.Close();
+        }
+    }
+}
